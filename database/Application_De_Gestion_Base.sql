c8375069feb7d6d898d8d592cd69addc9845aaf2
sbourbousse <54902460+sbourbousse@users.noreply.github.com>
2019-10-31 18:21:06 +0100

    Update Application_De_Gestion_Base.sql

--- database/Application_De_Gestion_Base.sql
+++ database/Application_De_Gestion_Base.sql
@@ -3,27 +3,32 @@
 -- Suppression des tables existantes --
 drop table if exists Produit;
 drop table if exists Rayon;
+drop table if exists Unite;
+drop table if exists User;
 
 -- Création des tables --
+create table Unite(
+	uniteId tinyint primary key,
+	uniteLibelle varchar(64)
+)engine=innodb charset=utf8;
+
 create table Produit(
-	produitId int primary key,
+	produitId int primary key auto_increment,
 	produitLibelle varchar(100),
 	produitDescription varchar(512),
 	produitPrixUnitaire float,
-	produitUnite varchar(5),
+	uniteId tinyint references Unite(uniteId),
 	produitImage varchar(128),
-	rayonId mediumInt
+	rayonId mediumInt references Rayon(rayonId)
 )engine=innodb charset=utf8;
 
 create table Rayon(
-	rayonId mediumint primary key,
-	rayonLibelle varchar(100)
+	rayonId mediumint primary key auto_increment,
+	rayonLibelle varchar(100),
+	rayonImage varchar(256)
 )engine=innodb charset=utf8;
 
 create table User(
 	userUsername varchar(64),
 	userPassword varchar(64)
 )engine=innodb charset=utf8;
-
-
-alter table Produit add foreign key Produit(rayonId) references Rayon(rayonId);
